f4cd229dde49fa0c55a37dfd6ed7e092bb17c7f9
Qi He <qhe@heyook.com>
2015-07-20 15:38:00 -0700

    add stripe key to test

--- config/environment.js
+++ config/environment.js
@@ -52,6 +52,8 @@ module.exports = function(environment) {
       proxyHost:  ''
     },
 
+    ENV.stripe.key = 'abc';
+
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
