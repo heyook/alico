343c1e0b0f2bf79cec406afdf120827b1165f935
Qi He <qhe@heyook.com>
2015-07-20 15:40:57 -0700

    set dov env

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     dotEnv: {
-      clientAllowedKeys: ['STRIPE_PK_KEY']
+      clientAllowedKeys: ['STRIPE_PK_KEY'],
+      path: {
+        development: './.env',
+        staging: './.env',
+        production: './.env.production'
+      }
     }
   });
 
